107400b2469947bae89c3fc1ed13d3071a883202
Derick Castillo <derick.castillo@flatironschool.com>
2019-10-05 23:21:44 +0000

    Automatically backed up by Learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -1,5 +1,7 @@
 def position_taken?(board, index)
-  if board[index] == " " || board[index] == ""
+ 
+  if board[index] == " " || board[index] == "" || board[index] == nil
     false
   end
+  
 end
